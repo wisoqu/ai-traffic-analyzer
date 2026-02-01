7c0727dcced7b877a5d15de38dbd510832240333
Mr. Optimist <georgiichurakov@outlook.com>
2026-02-01 21:13:55 +0700

    Optimized logs summarizing

--- app/parsers/ssh.py
+++ app/parsers/ssh.py
@@ -4,8 +4,8 @@ class SSHLogParser:
     def __init__(self):
         self.rejected = []
         self.accepted = []
-        self.rejected_ips = []
-        self.accepted_ips = []
+        self.rejected_ips = set()
+        self.accepted_ips = set()
 
         self.ip_pattern = re.compile(r"\b\d{1,3}(?:\.\d{1,3}){3}\b")
 
@@ -13,16 +13,16 @@ class SSHLogParser:
         for line in lines:
             if "Failed password" in line:
                 self.rejected.append(line)
-                self.rejected_ips.extend(self.ip_pattern.findall(line))
+                self.rejected_ips.update(self.ip_pattern.findall(line))
 
             elif "Accepted password" in line:
                 self.accepted.append(line)
-                self.accepted_ips.extend(self.ip_pattern.findall(line))
+                self.accepted_ips.update(self.ip_pattern.findall(line))
 
     def result(self) -> dict:
         return {
             "rejected_events" : len(self.rejected),
             "accepted_events" : len(self.accepted),
-            "rejected_ips" : list(set(self.rejected_ips)),
-            "accepted_ips" : list(set(self.accepted_ips)),
+            "rejected_ips" : list(self.rejected_ips),
+            "accepted_ips" : list(self.accepted_ips)
         }
